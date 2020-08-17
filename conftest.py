8ad9e638c64a37d47c7840334682bc1b29f00951
Curi-Bio-CI <69014632+Curi-Bio-CI@users.noreply.github.com>
2020-08-17 15:40:16 -0700

    Initialize master

---
+++ conftest.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""Pytest configuration."""
+import sys
+
+sys.dont_write_bytecode = True
---
+++ src/change_this_to_name_of_package/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""Docstring."""
+
+# from mymodule import example_function
+
+# __all__ = ['example_function']
